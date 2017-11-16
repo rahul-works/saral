8e4dd19f05cf1e7df35814d240c88cc144b7225c
rahul-works <rahul.thakur9985@gmail.com>
2017-11-16 19:58:32 +0530

    added bulk sms code.
    pfa

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -23,6 +23,50 @@ class Controller_Welcome extends Controller {
         die;
     }
     
+    public function action_testbulksms(){
+        //Your authentication key
+        $authKey = "f178866626475cebe22cb01c1873a77d";
+        //Multiple mobiles numbers separated by comma
+        $mobileNumber = "7259378545";
+        //Sender ID, While using route4 sender id should be 6 characters long.
+        $senderId = "102234";
+        //Your message to send, Add URL encoding here.
+        $message = urlencode("Test message its wokring 123-12");
+        //Define route 
+        $route = "B";
+        //Prepare you post parameters
+        $postData = array(
+            'authkey' => $authKey,
+            'mobiles' => $mobileNumber,
+            'message' => $message,
+            'sender' => $senderId,
+            'route' => $route
+        );
+        //API URL
+        $url = "http://sms.bulksmsserviceproviders.com/api/send_http.php";
+        // init the resource
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $postData
+            //,CURLOPT_FOLLOWLOCATION => true
+        ));
+        //Ignore SSL certificate verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        //get response
+        $output = curl_exec($ch);
+        //Print error if any
+        if (curl_errno($ch)) {
+            echo 'error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        echo $output;
+        die;
+    }
+    
     
 
 } // End Welcome
